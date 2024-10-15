2ef2d9a539e42d6ad4d586ab6ca14334acba407e
Hugo <magh080306hgtrzga2@cbtis217.edu.mx>
2024-10-15 07:33:26 -0600

    raices de una ecuación cuadrática

---
+++ ecuacion.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+	double a, b, c; cin >> a >> b >> c;
+	double d=pow(b,2)-(4*a*c);
+	if(d<0)
+	{
+		cout<<"NA";
+	}
+	else if(d==0)
+	{
+		double x=((b*-1)+sqrt(d))/(2*a);
+		cout<<x*1.0;
+	}
+	else
+	{
+		double x1=((b*-1)+sqrt(d))/(2*a);
+		double x2=((b*-1)-sqrt(d))/(2*a);
+		if(x1<x2)
+		{
+			cout<<x1*1.0<<endl<<x2*1.0;
+		}
+		else
+		{
+			cout<<x2*1.0<<endl<<x1*1.0;
+		}
+	}
+}
